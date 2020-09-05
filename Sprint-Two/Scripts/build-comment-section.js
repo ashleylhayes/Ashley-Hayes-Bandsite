63aa461c951b74ce50e9c5c8a6687e7909695e57
Ashley Hayes <ashley_hayes@hotmail.ca>
2020-09-05 12:36:24 -0600

    re-created comments with JavaScript

---
+++ Sprint-Two/Scripts/build-comment-section.js
@@ -0,0 +1,143 @@
+//create comment container
+let commentContainer1 = document.createElement('div');
+//gave comment container class name
+commentContainer1.className = "comments__comment-container-1";
+//append comment container to comments section
+document.querySelector('.comments').appendChild(commentContainer1);
+
+//create avatar container
+let avatarContainer1 = document.createElement('div');
+//give class
+avatarContainer1.className = "comments__avatar-container";
+//append
+document.querySelector('.comments__comment-container-1').appendChild(avatarContainer1);
+
+//create comment info container
+let commentInfoContainer1 = document.createElement('div');
+//give class name
+commentInfoContainer1.className = "comments__comment-info-container";
+//append
+document.querySelector('.comments__comment-container-1').appendChild(commentInfoContainer1);
+
+//create name
+let commentName1 = document.createElement('p');
+//give class
+commentName1.className = "comments__name";
+//add content
+commentName1.innerText = "Michael Lyons";
+//append
+document.querySelector('.comments__comment-info-container').appendChild(commentName1);
+
+//create timestamp
+let timestamp1 = document.createElement('p');
+//give class
+timestamp1.className = "comments__timestamp";
+//add content
+timestamp1.innerText = "12/18/2018";
+//append
+document.querySelector('.comments__comment-info-container').appendChild(timestamp1);
+
+//create comment
+let comment1 = document.createElement('p');
+//give class
+comment1.className = "comments__comment";
+//add content
+comment1.innerText = "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.";
+//append
+document.querySelector('.comments__comment-info-container').appendChild(comment1);
+
+//create comment container
+let commentContainer2 = document.createElement('div');
+//gave comment container class name
+commentContainer2.className = "comments__comment-container-2";
+//append comment comtainer to comments section
+document.querySelector('.comments').appendChild(commentContainer2);
+
+//create avatar container
+let avatarContainer2 = document.createElement('div');
+//give class
+avatarContainer2.className = "comments__avatar-container";
+//append
+document.querySelector('.comments__comment-container-2').appendChild(avatarContainer2);
+
+//create comment info container
+let commentInfoContainer2 = document.createElement('div');
+//give class name
+commentInfoContainer2.className = "comments__comment-info-container-2";
+//append
+document.querySelector('.comments__comment-container-2').appendChild(commentInfoContainer2);
+
+//create name
+let commentName2 = document.createElement('p');
+//give class
+commentName2.className = "comments__name";
+//add content
+commentName2.innerText = "Gary Wong";
+//append
+document.querySelector('.comments__comment-info-container-2').appendChild(commentName2);
+
+//create timestamp
+let timestamp2 = document.createElement('p');
+//give class
+timestamp2.className = "comments__timestamp";
+//add content
+timestamp2.innerText = "12/12/2018";
+//append
+document.querySelector('.comments__comment-info-container-2').appendChild(timestamp2);
+
+//create comment
+let comment2 = document.createElement('p');
+//give class
+comment2.className = "comments__comment";
+//add content
+comment2.innerText = "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!";
+//append
+document.querySelector('.comments__comment-info-container-2').appendChild(comment2);
+
+//create comment container
+let commentContainer3 = document.createElement('div');
+//gave comment container class name
+commentContainer3.className = "comments__comment-container-3";
+//append comment comtainer to comments section
+document.querySelector('.comments').appendChild(commentContainer3);
+
+//create avatar container
+let avatarContainer3 = document.createElement('div');
+//give class
+avatarContainer3.className = "comments__avatar-container";
+//append
+document.querySelector('.comments__comment-container-3').appendChild(avatarContainer3);
+
+//create comment info container
+let commentInfoContainer3 = document.createElement('div');
+//give class name
+commentInfoContainer3.className = "comments__comment-info-container-3";
+//append
+document.querySelector('.comments__comment-container-3').appendChild(commentInfoContainer3);
+
+//create name
+let commentName3 = document.createElement('p');
+//give class
+commentName3.className = "comments__name";
+//add content
+commentName3.innerText = "Theodore Duncan";
+//append
+document.querySelector('.comments__comment-info-container-3').appendChild(commentName3);
+
+//create timestamp
+let timestamp3 = document.createElement('p');
+//give class
+timestamp3.className = "comments__timestamp";
+//add content
+timestamp3.innerText = "11/15/2018";
+//append
+document.querySelector('.comments__comment-info-container-3').appendChild(timestamp3);
+
+//create comment
+let comment3 = document.createElement('p');
+//give class
+comment3.className = "comments__comment";
+//add content
+comment3.innerText = "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!";
+//append
+document.querySelector('.comments__comment-info-container-3').appendChild(comment3);
